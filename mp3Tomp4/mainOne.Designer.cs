920c0e0dece5e12a867d1f09dbcd01da1c3df03c
AAVision <75311052+AAVision@users.noreply.github.com>
2021-09-04 21:16:50 +0300

    General Fix!

--- mp3Tomp4/mainOne.Designer.cs
+++ mp3Tomp4/mainOne.Designer.cs
@@ -83,12 +83,13 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
             this.button2.Location = new System.Drawing.Point(558, 0);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(38, 37);
+            this.button2.Size = new System.Drawing.Size(38, 34);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -96,12 +97,13 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.Location = new System.Drawing.Point(602, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(38, 37);
+            this.button1.Size = new System.Drawing.Size(38, 34);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
